f5a59741c8285ce90d26d66066448c57fdad94bb
Dave Bonan <55212683+dbonan@users.noreply.github.com>
2021-06-24 09:47:22 -0700

    Add files via upload

--- vertical_grid.py
+++ vertical_grid.py
@@ -1,7 +1,20 @@
 import numpy as np
 
-def vertical_grid(dz,h):
-
+def vertical_grid(h,dz):
+    """
+    Description:
+        Computes vertical grid for column model.
+        
+    Input:
+        h   : depth of ocean                              [ m ]
+        dz  : size of grid box                            [ m ]
+        
+    Output:
+        zt  : vertical grid for temperature/salinity      [ m ]
+        zw  : vertical grid for velocity                  [ m ]
+        nzt : size of temperature/salinity vertical grid
+        nzw : size of velocity vertical grid 
+    """
     nzt = int(h/dz)
     nzw = nzt + 1
     zt  = np.arange(-h, 0, dz) + dz/2
