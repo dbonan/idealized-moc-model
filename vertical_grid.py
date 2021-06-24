74e06960ba65c2c02d579269142f265e56ca7df0
Dave Bonan <55212683+dbonan@users.noreply.github.com>
2021-06-24 09:41:41 -0700

    Add files via upload

---
+++ vertical_grid.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def vertical_grid(dz,h,):
+
+    nzt = int(h/dz)
+    nzw = nzt + 1
+    zt  = np.arange(-h, 0, dz) + dz/2
+    zw  = np.arange(-h, dz, dz)
+    return zt, zw, nzt, nzw
